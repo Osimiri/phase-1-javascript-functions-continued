385e1af8666268839ed1dcd3990b8141b8d2b2d3
Justin Elue <mjuelue@gmail.com>
2023-01-12 23:20:59 -0600

    what a lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun();
+saturdayFun("ice-skate");
+
+function mondayWork(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`;
+}
+
+/*
+function wrapAdjective(bold = "*"){
+     const innerFunction = function(bold){
+        return `You are ${bold} ${string} ${bold}!`;
+    }
+    return wrapAdjectived();
+}
+*/
+
+
+
+function wrapAdjective (x = "*") {
+    const output = input => `You are ${x}${input}${x}!`;
+    return output;
+}
